85a0882d69bce5920e7a3d05ea9c519b59c4a430
Anjan Shrestha <anjhero@Anjans-MacBook-Pro.local>
2024-08-16 14:43:45 +0545

    practice

--- lib/functionsandparameters.dart
+++ lib/functionsandparameters.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 // void main()
 // {
 //  print(myfunc());
@@ -63,37 +64,143 @@
     // }
 
 
-class Mathematics{
-  int addition(int num1, int num2){
-    return num1 + num2 ;
+// class Mathematics{
+//   int addition(int num1, int num2){
+//     return num1 + num2 ;
 
-  }
+//   }
 
-  int subtract( int num1 , int num2){
-    return num1 - num2 ;
-  }
+//   int subtract( int num1 , int num2){
+//     return num1 - num2 ;
+    
+//   }
   
-  int multiply(int num1, int num2){
-    return num1*num2;
-  }
+//   int multiply(int num1, int num2){
+//     return num1*num2;
+//   }
 
-  int division(int num1 , int num2){
-    return num1~/num2 ;
-  }
+//   int division(int num1 , int num2){
+//     return num1~/num2 ;
+//   }
 
-}
+// }
+
+// void main() {
+//   Mathematics mathematics =  Mathematics();
+// int num1 = 1000;
+// int num2 = 2;
+
+// int addresult = mathematics.addition(num1, num2);
+// print(addresult);
+// int subresult = mathematics.subtract(num1, num2);
+// print(subresult);
+// int mulresult = mathematics.multiply(num1, num2);
+// print(mulresult);
+// int divresult = mathematics.division(num1, num2);
+// print(divresult);
+// }
+
+
+
+// A person. The implicit interface contains greet().
+// class Person {
+//   // In the interface, but visible only in this library.
+//   final String _name;
+
+//   // Not in the interface, since this is a constructor.
+//   Person(this._name);
+
+//   // In the interface.
+//   String greet(String who) => 'Hello, $who. I am $_name.';
+// }
+
+// // An implementation of the Person interface.
+// class Impostor implements Person {
+//   String get _name => '';
+
+//   String greet(String who) => 'Hi $who. Do you know who I am?';
+// }
+
+// String greetBob(Person person) => person.greet('Bob');
+
+// void main() {
+//   print(greetBob(Person('Kathy')));
+//   print(greetBob(Impostor()));
+// }
+
+
+
+
+
+
+
+
+// class Human {
+//   String name;
+//   String address;
+//   String gender;
+
+//   // Constructor
+//   Human({required this.name, required this.address, required this.gender});
+
+//   // Function to ask for information
+//   void ask() {
+//     stdout.write('Enter your name: ');
+//     name = stdin.readLineSync() ?? '';
+
+//     stdout.write('Enter your address: ');
+//     address = stdin.readLineSync() ?? '';
+
+//     stdout.write('Enter your gender: ');
+//     gender = stdin.readLineSync() ?? '';
+//   }
+
+//   // Function to display the information
+//   void display() {
+//     print('Name: $name');
+//     print('Address: $address');
+//     print('Gender: $gender');
+//   }
+// }
+
+// void main() {
+//   Human person = Human(name: '', address: '', gender: '');
+//   person.ask();
+//   person.display();
+//    }
+
+
+
+// class Maths{
+//   int num1;
+//   int num2;
+  
+//   Maths({required this.num1, required this.num2});
+
+//   void result (){
+//     stdout.write("enter the first number");
+//      String? num1 = stdin.readLineSync();
+
+
+//   }
+
+// }
+
+//  void main(){
+
+
+//  }
+
 
 void main() {
-  Mathematics mathematics =  Mathematics();
-int num1 = 1000;
-int num2 = 2;
-
-int addresult = mathematics.addition(num1, num2);
-print(addresult);
-int subresult = mathematics.subtract(num1, num2);
-print(subresult);
-int mulresult = mathematics.multiply(num1, num2);
-print(mulresult);
-int divresult = mathematics.division(num1, num2);
-print(divresult);
+  stdout.write('Enter an integer: ');
+  String? input = stdin.readLineSync();
+  
+  if (input != null) {
+    int number = int.parse(input);
+    print('You entered: $number');
+  } else {
+    print('No input received.');
+  }
 }
+dddfewwßàźxč
